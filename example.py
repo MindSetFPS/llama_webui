b70ac6fbd15ce9b77e31e07e04cdc6ec58d698c3
Daniel <daanielescobar@gmail.com>
2023-03-06 09:29:49 +0000

    move generator

--- example.py
+++ example.py
@@ -9,11 +9,8 @@ import fire
 import time
 import json
 import pyarrow as pa
-
 from pathlib import Path
-
 from llama import ModelArgs, Transformer, Tokenizer, LLaMA
-
 
 def load(
     ckpt_dir: str,
@@ -77,56 +74,5 @@ def load(
     print(f"Loaded in {time.time() - start_time:.2f} seconds")
     return generator
 
+generator = load(ckpt_dir="/home/llama/models/7B", tokenizer_path="/home/llama/models/tokenizer.model", max_seq_len=512, max_batch_size=16)
 
-def main(
-    ckpt_dir: str,
-    tokenizer_path: str,
-    temperature: float = 0.8,
-    top_p: float = 0.95,
-    max_seq_len: int = 512,
-    max_batch_size: int = 16,
-):
-    generator = load(ckpt_dir, tokenizer_path, max_seq_len, max_batch_size)
-
-    prompts = [
-        # For these prompts, the expected answer is the natural continuation of the prompt
-        "I believe the meaning of life is",
-        "Simply put, the theory of relativity states that ",
-        "Building a website can be done in 10 simple steps:\n",
-        # Few shot prompts: https://huggingface.co/blog/few-shot-learning-gpt-neo-and-inference-api
-        """Tweet: "I hate it when my phone battery dies."
-Sentiment: Negative
-###
-Tweet: "My day has been 👍"
-Sentiment: Positive
-###
-Tweet: "This is the link to the article"
-Sentiment: Neutral
-###
-Tweet: "This new music video was incredibile"
-Sentiment:""",
-        """Translate English to French:
-
-sea otter => loutre de mer
-
-peppermint => menthe poivrée
-
-plush girafe => girafe peluche
-
-cheese =>""",
-    ]
-    results = generator.generate(
-        prompts, max_gen_len=max_seq_len, temperature=temperature, top_p=top_p
-    )
-
-    for result in results:
-        print("\n==================================\n")
-        print(result)
-        print("\n==================================\n")
-
-    with open('example-output.json', 'w') as f:
-        json.dump(results, f)
-
-
-if __name__ == "__main__":
-    fire.Fire(main)
